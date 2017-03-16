b9509fe5ddb1b2befb9381c609e7ad09d722256b
Jim Lambert <jim.lambert@gmail.com>
2017-03-16 09:36:17 -0400

    latest

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # take the hit and update all.
-sudo yum -y update all
+sudo yum -y update 
 
 sudo yum -y install java
 sudo yum -y install emacs
@@ -12,19 +12,21 @@ sudo yum -y install python-setuptools
 sudo yum -y install python-devel
 sudo easy_install pip
 
-## python3
+# python3 /usr/local/bin/python3
 sudo yum install yum-utils
 sudo yum -y install wget
 sudo yum -y install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel
-wget "https://www.python.org/ftp/python/3.5.3/Python-3.5.3.tgz"
+wget --no-clobber  "https://www.python.org/ftp/python/3.5.3/Python-3.5.3.tgz"
+tar xzvf Python-3.5.3.tgz
 cd Python-3.5.3
-sudo ./configure
-sudo make
+./configure
+make
 sudo make install
-# install python3 setuptools
-curl https://bitbucket.org/pypa/setuptools/downloads/ez_setup.py | sudo python3 -
-# install pip
-curl https://bootstrap.pypa.io/get-pip.py | sudo python3 -
+cd
+# install python3 pip and setuptools
+wget --no-clobber "https://bootstrap.pypa.io/get-pip.py" 
+sudo /usr/local/bin/python3 get-pip.py
+
 
 sudo yum -y install mlocate
 sudo updatedb
@@ -45,5 +47,11 @@ yum -y install dsc20
 sudo chkconfig --add cassandra
 sudo chkconfig --levels 2345 cassandra on
 
+
 # Cassandra python driver
-pip install cassandra-driver
+sudo pip install cassandra-driver
+sudo /usr/local/bin/python3 -m pip install cassandra-driver
+
+# start up cassandra
+sudo service cassandra start
+sudo service cassandra status
